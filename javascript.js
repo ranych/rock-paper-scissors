bf0e54cabdf67d79678cf3d3ac7f2dc769150ecc
Randy Chung <108911643+ranych@users.noreply.github.com>
2023-12-25 13:15:27 -0500

    Add some pseudocode comments to JS file
    Have a clear idea of how to start before jumping into coding it

---
+++ javascript.js
@@ -0,0 +1,20 @@
+// Loop five times
+    // Prompt user to make a choice
+    // Computer randomly makes a choice
+    /* If one of the following: user chooses rock and computer chooses scissors OR user
+       chooses paper and computer chooses rock OR user chooses scissors and computer
+       chooses paper, then... */
+        // User wins. Your selection beats computer selection
+        // Add one to user score.
+    /* If one of the following: user chooses rock and computer chooses paper OR user
+       chooses paper and computer chooses scissors OR user chooses scissors and computer
+       chooses rock, then... */
+        // User loses. Computer selection beats your selection
+        // Add one to computer score
+    // If user and computer are tied
+        // Play reround
+
+// If user has more points than computer
+    // Display message that user is the winner
+// If computer has more points than user
+    // Display message that computer is the winner
