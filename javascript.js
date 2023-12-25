80890d8cd808c2391463eb1772c44f78a8af044f
Randy Chung <108911643+ranych@users.noreply.github.com>
2023-12-25 18:07:13 -0500

    Rearrange functions and calls accordingly
    This is to ensure that it is organized into what is reusable or not

--- javascript.js
+++ javascript.js
@@ -1,11 +1,5 @@
 // Loop five times
-    // Set variable for a list of choices
-    const choice = ["Rock", "Paper", "Scissors"];
-
-    // Prompt user to make a choice
-    let playerSelection = prompt("Please choose rock, paper, or scissors: ");
-    selectionChecker(playerSelection.toLowerCase());
-
+    // Checks if one of three choices is selected
     function selectionChecker(playerChoice) {
         while(true) {
             if(playerChoice === ( "rock" || "paper" || "scissors" )) {
@@ -18,27 +12,59 @@
         }
     }
 
+    // Computer randomly makes a choice from three strings
+    function getComputerChoice() {
+        const choice = ["Rock", "Paper", "Scissors"];
+        let index = Math.floor(Math.random() * choice.length);
+        console.log(index);
+        let computerSelection = choice[index];
+        console.log(computerSelection);
+    }
+
+    // Set variables to keep track of score
+    let userScore;
+    let computerScore;
+
+    function playRound(playerSelection, computerSelection) {
+        /* If one of the following: user chooses rock and computer chooses scissors OR user
+        chooses paper and computer chooses rock OR user chooses scissors and computer
+        chooses paper, then... */
+            // User wins. Your selection beats computer selection
+            // Add one to user score.
+        if( (playerSelection === "rock" && computerSelection === "scissors") ||
+            (playerSelection === "paper" && computerSelection === "rock") ||
+            (playerSelection === "scissors" && computerSelection === "paper") ) {
+                console.log("User wins! Your selection beats computer selection.");
+                userScore += 1;
+        }
+
+        /* If one of the following: user chooses rock and computer chooses paper OR user
+        chooses paper and computer chooses scissors OR user chooses scissors and computer
+        chooses rock, then... */
+            // User loses. Computer selection beats your selection
+            // Add one to computer score
+        else if( (playerSelection === "rock" && computerSelection === "paper") ||
+            (playerSelection === "paper" && computerSelection === "scissors") ||
+            (playerSelection === "scissors" && computerSelection === "rock") ) {
+                console.log("User loses! Computer selection beats your selection.");
+                computerScore += 1;
+        }
+
+        // If user and computer are tied
+            // Play reround
+        else {
+            let playerSelection = prompt("Please choose rock, paper, or scissors: ");
+            selectionChecker(playerSelection.toLowerCase());
+        }
+    }
+
+    // Game play program
+    let playerSelection = prompt("Please choose rock, paper, or scissors: ");
+    selectionChecker(playerSelection.toLowerCase());
     console.log(playerSelection);
-
-    // Computer randomly makes a choice
-    let index = Math.floor(Math.random() * choice.length);
-    console.log(index);
-    let computerSelection = choice[index];
+    let computerSelection = getComputerChoice();
     console.log(computerSelection);
-
-    /* If one of the following: user chooses rock and computer chooses scissors OR user
-       chooses paper and computer chooses rock OR user chooses scissors and computer
-       chooses paper, then... */
-        // User wins. Your selection beats computer selection
-        // Add one to user score.
-    
-    /* If one of the following: user chooses rock and computer chooses paper OR user
-       chooses paper and computer chooses scissors OR user chooses scissors and computer
-       chooses rock, then... */
-        // User loses. Computer selection beats your selection
-        // Add one to computer score
-    // If user and computer are tied
-        // Play reround
+    playRound(playerSelection.toLowerCase(), computerSelection.toLowerCase());
 
 // If user has more points than computer
     // Display message that user is the winner
