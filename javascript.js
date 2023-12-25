7ef02b115bf045e8a95bc751c1be60ea694be894
Randy Chung <108911643+ranych@users.noreply.github.com>
2023-12-25 17:21:20 -0500

    Choose a string from a list at random for computer
    Built in functions were used to help pick an index out of the choices

--- javascript.js
+++ javascript.js
@@ -1,6 +1,36 @@
 // Loop five times
+    // Set variable for a list of choices
+    const choice = ["Rock", "Paper", "Scissors"];
+
     // Prompt user to make a choice
+    // try {
+        let playerSelection = prompt("Please choose rock, paper, or scissors: ");
+        selectionChecker(playerSelection.toLowerCase());
+
+        function selectionChecker(playerChoice) {
+            while(true) {
+                if(playerChoice === ( "rock" || "paper" || "scissors" )) {
+                    return playerChoice;
+                } else {
+                    console.log("Please choose one from the three options provided");
+                    playerChoice = prompt("Please choose rock, paper, or scissors: ");
+                    true;
+                }
+            }
+        }
+
+        console.log(playerSelection);
+    // }
+    /* catch(e) {
+        console.error(e);
+    } */
+
     // Computer randomly makes a choice
+    let index = Math.floor(Math.random() * choice.length);
+    console.log(index);
+    let computerSelection = choice[index];
+    console.log(computerSelection);
+
     /* If one of the following: user chooses rock and computer chooses scissors OR user
        chooses paper and computer chooses rock OR user chooses scissors and computer
        chooses paper, then... */
